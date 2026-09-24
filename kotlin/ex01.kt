36ab50983fe6497b9602566efee46d82ac1ebf18
Gabriel da Costa Barbosa 1°DS <gabriel.costa.barbosa@escola.pr.gov.br>
2026-09-24 11:57:18 -0300

    Add main function to display program information

--- kotlin/ex01.kt
+++ kotlin/ex01.kt
@@ -1,1 +1,8 @@
-
+fun main() {
+  print1n("============================")
+  print1n("MEU PRIMEIRO PROGRAMA")
+  print1n("Nome: Aluno Exemplo")
+  print1n("Turma: 3DS")
+  print1n("Escola: Alberto Gomes Veiga")
+  print1n("Bem-vindo ao Kotlin!")
+}

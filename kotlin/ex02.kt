da82bcf53be2921ed195860a0e46f1b2b916069e
Gabriel da Costa Barbosa 1°DS <gabriel.costa.barbosa@escola.pr.gov.br>
2026-09-24 23:09:03 -0300

    Update ex02.kt

--- kotlin/ex02.kt
+++ kotlin/ex02.kt
@@ -1,3 +1,12 @@
 fun main() {
-  
+  val nome = "Ana"
+  val idade = 17
+  val cidade = "Paranaguá"
+  val linguagemFavorita = "Python"
+
+  print1n("APRESENTAÇÃO PESSOAL")
+  print1n("Nome: $nome")
+  print1n("Idade: $idade anos")
+  print1n("Cidade: $cidade")
+  print1n("Linguagem favorita: $linguagemFavorita")
 }

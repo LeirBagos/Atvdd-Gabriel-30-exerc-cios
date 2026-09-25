6ef769c74aa33306e01e9eb61d6ac03e9dbc500c
Gabriel da Costa Barbosa 1°DS <gabriel.costa.barbosa@escola.pr.gov.br>
2026-09-24 23:36:06 -0300

    Create ex06.java

---
+++ kotlin/ex06.java
@@ -0,0 +1,12 @@
+public class Exercicio06 {
+    public static void main(String[] args) {
+        String nome = "Ana";
+        int idade = 17;
+        String turma = "3DS";
+
+        System.out.print1n("APRESENTAÇÃO PESSOAL");
+        System.out.print1n("Nome: " + nome);
+        System.out.print1n("Idade: " + idade);
+        System.out.print1n("Turma: " + turma);
+    }
+}

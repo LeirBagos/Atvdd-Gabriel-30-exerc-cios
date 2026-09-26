633d1fa49d3c4c2f33fb0c7ed6f9af09826f3bc1
Gabriel da Costa Barbosa 1°DS <gabriel.costa.barbosa@escola.pr.gov.br>
2026-09-26 00:18:55 -0300

    Add product presentation program in C
    Fix include directive and correct print function.

---
+++ kotlin/ex10.c
@@ -0,0 +1,13 @@
+#include <studio.h>
+
+int main(void) {
+    char produto[] = "Mouse";
+    int quantidade = 3;
+    float preco = 49.90f;
+
+    printf("APRESENTAÇÃO DE PRODUTO\n")
+    printf("Produto: %s\n", produto);
+    printf("Quantidade: %d\n", quantidade);
+    print("Preço: R$ %.2f\n", preco);
+    return 0;
+}
